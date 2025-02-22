f633e0dd4b9866f6be52248c341b98c19965e79d
Victor Gael <ack3stx10@gmail.com>
2025-02-22 11:31:47 -0600

    interceptos y servicios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Views/auth/login/login.component';
 import { RegisterComponent } from './Views/auth/register/register.component';
-
+import { WelcomeComponent } from './Views/welcome/welcome.component';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
-    }
-    ,
+    },
     {
         path: 'register',
         component: RegisterComponent
     },
     {
         path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
-    }
-
-    
-];
+        component: WelcomeComponent
+    },
+]
